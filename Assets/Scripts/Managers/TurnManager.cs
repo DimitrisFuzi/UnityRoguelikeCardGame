a33fd4ec7a8e83edca338ff02e20692091d9d4de
FuZi.D <foutz_is@hotmail.com>
2025-06-14 16:26:52 +0300

    added End Turn Button and Enemy Turn

--- Assets/Scripts/Managers/TurnManager.cs
+++ Assets/Scripts/Managers/TurnManager.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
-using System;
 
 public class TurnManager : MonoBehaviour
 {
     public static TurnManager Instance { get; private set; }
 
     public event Action OnPlayerTurnStart;
+    public event Action OnPlayerTurnEnd;
     public event Action OnEnemyTurnStart;
+    public event Action OnEnemyTurnEnd;
 
-    private EnemyManager enemyManager;
-    private PlayerManager playerManager;
+    [Header("References")]
+    [SerializeField] private EnemyManager enemyManager;
+    [SerializeField] private PlayerManager playerManager;
+
+    public bool IsPlayerTurn { get; private set; }
 
     void Awake()
     {
@@ -26,28 +31,46 @@ public class TurnManager : MonoBehaviour
 
     void Start()
     {
-        enemyManager = GetComponent<EnemyManager>();
-        playerManager = GetComponent<PlayerManager>();
+        if (enemyManager == null)
+            enemyManager = FindFirstObjectByType<EnemyManager>();
+
+        if (playerManager == null)
+            playerManager = FindFirstObjectByType<PlayerManager>();
+
+        StartPlayerTurn();
     }
 
     public void StartPlayerTurn()
     {
-        Debug.Log("🎮 Player Turn!");
+        Debug.Log("🎮 Player Turn Started!");
+        IsPlayerTurn = true;
+
         OnPlayerTurnStart?.Invoke();
     }
 
     public void EndPlayerTurn()
     {
-        Debug.Log("👿 Enemies Turn!");
-        OnEnemyTurnStart?.Invoke();
+        Debug.Log("🎮 Player Turn Ended!");
+        IsPlayerTurn = false;
+
+        OnPlayerTurnEnd?.Invoke();
+
         StartCoroutine(EnemyTurn());
     }
 
     private IEnumerator EnemyTurn()
     {
+        Debug.Log("👿 Enemy Turn Started!");
+        OnEnemyTurnStart?.Invoke();
+
         yield return new WaitForSeconds(1f);
+
         enemyManager.PerformEnemyActions();
+
         yield return new WaitForSeconds(1f);
+
+        Debug.Log("👿 Enemy Turn Ended!");
+        OnEnemyTurnEnd?.Invoke();
 
         StartPlayerTurn();
     }
---
+++ Assets/Scripts/UI/EndTurnButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTurnButton : MonoBehaviour
+{
+    [SerializeField] private Button endTurnButton;
+
+    private void Start()
+    {
+        if (endTurnButton == null)
+            endTurnButton = GetComponent<Button>();
+
+        if (endTurnButton != null)
+            endTurnButton.onClick.AddListener(EndTurn);
+        else
+            Debug.LogError("EndTurnButton: No Button component assigned.");
+    }
+
+    private void EndTurn()
+    {
+        Debug.Log("⏭️ End Turn Button Pressed");
+        TurnManager.Instance?.EndPlayerTurn();
+    }
+}
